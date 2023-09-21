2d478475b8711a58c4115b6bb519a857c093e167
Claudius Ramos <claudiusramos@gmail.com>
2023-09-20 22:49:47 -0400

    teste

--- server.js
+++ server.js
@@ -25,6 +25,18 @@ server.post('/videos', async (request, reply) => {
 
 })
 
+server.get('videos', async (request) => {
+    const search = request.query.search
+
+    console.log(search)
+
+    const videos = await database.list(search)
+
+
+
+    return videos
+})
+
 server.get('/videos', async (request) => {
     const search = request.query.search
 
