0f7c15b876f1350ff9699e7060e215357267c729
Claudius Ramos <claudiusramos@gmail.com>
2023-09-20 22:58:23 -0400

    corrigindo path

--- server.js
+++ server.js
@@ -25,17 +25,7 @@ server.post('/videos', async (request, reply) => {
 
 })
 
-server.get('videos', async (request) => {
-    const search = request.query.search
 
-    console.log(search)
-
-    const videos = await database.list(search)
-
-
-
-    return videos
-})
 
 server.get('/videos', async (request) => {
     const search = request.query.search
